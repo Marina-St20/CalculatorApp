afe1139d24e71f4021d9ea4e9db9e4326cfb215d
Marina Scott <ironduckmei@gmail.com>
2025-03-22 22:14:56 -0600

    Created a basic layout for the Calculator App

--- CalculatorApp/CalculatorApp/Form1.Designer.cs
+++ CalculatorApp/CalculatorApp/Form1.Designer.cs
@@ -31,45 +31,291 @@
             button1 = new Button();
             label1 = new Label();
             textBox1 = new TextBox();
+            backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            button10 = new Button();
+            button11 = new Button();
+            button12 = new Button();
+            button13 = new Button();
+            button14 = new Button();
+            button15 = new Button();
+            button16 = new Button();
+            button17 = new Button();
+            button18 = new Button();
+            button19 = new Button();
+            button20 = new Button();
             SuspendLayout();
             // 
             // button1
             // 
             button1.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
-            button1.Location = new Point(24, 107);
+            button1.Location = new Point(12, 164);
             button1.Name = "button1";
-            button1.Size = new Size(606, 34);
+            button1.Size = new Size(94, 82);
             button1.TabIndex = 0;
-            button1.Text = "Click me";
+            button1.Text = "BKSP";
+            button1.UseMnemonic = false;
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(24, 9);
+            label1.Location = new Point(184, 93);
             label1.Name = "label1";
-            label1.Size = new Size(190, 25);
+            label1.Size = new Size(0, 25);
             label1.TabIndex = 1;
-            label1.Text = "This is a caluclator app";
-            label1.Click += label1_Click;
             // 
             // textBox1
             // 
-            textBox1.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
-            textBox1.Location = new Point(24, 37);
+            textBox1.Location = new Point(12, 12);
+            textBox1.Multiline = true;
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(606, 31);
+            textBox1.Size = new Size(459, 133);
             textBox1.TabIndex = 2;
-            textBox1.Text = "enter here";
-            textBox1.TextChanged += textBox1_TextChanged;
+            textBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            button2.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            button2.Location = new Point(133, 164);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 82);
+            button2.TabIndex = 3;
+            button2.Text = "(";
+            button2.UseMnemonic = false;
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            button3.Location = new Point(255, 164);
+            button3.Name = "button3";
+            button3.Size = new Size(94, 82);
+            button3.TabIndex = 4;
+            button3.Text = ")";
+            button3.UseMnemonic = false;
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            button4.Location = new Point(377, 164);
+            button4.Name = "button4";
+            button4.Size = new Size(94, 82);
+            button4.TabIndex = 5;
+            button4.Text = "/";
+            button4.UseMnemonic = false;
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            button5.Location = new Point(12, 272);
+            button5.Name = "button5";
+            button5.Size = new Size(94, 82);
+            button5.TabIndex = 6;
+            button5.Text = "7";
+            button5.UseMnemonic = false;
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            button6.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            button6.Location = new Point(133, 272);
+            button6.Name = "button6";
+            button6.Size = new Size(94, 82);
+            button6.TabIndex = 7;
+            button6.Text = "8";
+            button6.UseMnemonic = false;
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            button7.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            button7.Location = new Point(255, 272);
+            button7.Name = "button7";
+            button7.Size = new Size(94, 82);
+            button7.TabIndex = 8;
+            button7.Text = "9";
+            button7.UseMnemonic = false;
+            button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            button8.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            button8.Location = new Point(377, 272);
+            button8.Name = "button8";
+            button8.Size = new Size(94, 82);
+            button8.TabIndex = 9;
+            button8.Text = "x";
+            button8.UseMnemonic = false;
+            button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            button9.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            button9.Location = new Point(12, 381);
+            button9.Name = "button9";
+            button9.Size = new Size(94, 82);
+            button9.TabIndex = 10;
+            button9.Text = "4";
+            button9.UseMnemonic = false;
+            button9.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            button10.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            button10.Location = new Point(133, 381);
+            button10.Name = "button10";
+            button10.Size = new Size(94, 82);
+            button10.TabIndex = 11;
+            button10.Text = "5";
+            button10.UseMnemonic = false;
+            button10.UseVisualStyleBackColor = true;
+            // 
+            // button11
+            // 
+            button11.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            button11.Location = new Point(255, 381);
+            button11.Name = "button11";
+            button11.Size = new Size(94, 82);
+            button11.TabIndex = 12;
+            button11.Text = "6";
+            button11.UseMnemonic = false;
+            button11.UseVisualStyleBackColor = true;
+            // 
+            // button12
+            // 
+            button12.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            button12.Location = new Point(377, 381);
+            button12.Name = "button12";
+            button12.Size = new Size(94, 82);
+            button12.TabIndex = 13;
+            button12.Text = "-";
+            button12.UseMnemonic = false;
+            button12.UseVisualStyleBackColor = true;
+            // 
+            // button13
+            // 
+            button13.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            button13.Location = new Point(12, 490);
+            button13.Name = "button13";
+            button13.Size = new Size(94, 82);
+            button13.TabIndex = 14;
+            button13.Text = "1";
+            button13.UseMnemonic = false;
+            button13.UseVisualStyleBackColor = true;
+            // 
+            // button14
+            // 
+            button14.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            button14.Location = new Point(133, 490);
+            button14.Name = "button14";
+            button14.Size = new Size(94, 82);
+            button14.TabIndex = 15;
+            button14.Text = "2";
+            button14.UseMnemonic = false;
+            button14.UseVisualStyleBackColor = true;
+            // 
+            // button15
+            // 
+            button15.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            button15.Location = new Point(255, 490);
+            button15.Name = "button15";
+            button15.Size = new Size(94, 82);
+            button15.TabIndex = 16;
+            button15.Text = "3";
+            button15.UseMnemonic = false;
+            button15.UseVisualStyleBackColor = true;
+            // 
+            // button16
+            // 
+            button16.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            button16.Location = new Point(377, 490);
+            button16.Name = "button16";
+            button16.Size = new Size(94, 82);
+            button16.TabIndex = 17;
+            button16.Text = "+";
+            button16.UseMnemonic = false;
+            button16.UseVisualStyleBackColor = true;
+            // 
+            // button17
+            // 
+            button17.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            button17.Location = new Point(12, 598);
+            button17.Name = "button17";
+            button17.Size = new Size(94, 82);
+            button17.TabIndex = 18;
+            button17.Text = "0";
+            button17.UseMnemonic = false;
+            button17.UseVisualStyleBackColor = true;
+            // 
+            // button18
+            // 
+            button18.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            button18.Location = new Point(133, 598);
+            button18.Name = "button18";
+            button18.Size = new Size(94, 82);
+            button18.TabIndex = 19;
+            button18.Text = ".";
+            button18.UseMnemonic = false;
+            button18.UseVisualStyleBackColor = true;
+            // 
+            // button19
+            // 
+            button19.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            button19.Location = new Point(255, 598);
+            button19.Name = "button19";
+            button19.Size = new Size(94, 82);
+            button19.TabIndex = 20;
+            button19.Text = "e";
+            button19.UseMnemonic = false;
+            button19.UseVisualStyleBackColor = true;
+            // 
+            // button20
+            // 
+            button20.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            button20.Location = new Point(377, 598);
+            button20.Name = "button20";
+            button20.Size = new Size(94, 82);
+            button20.TabIndex = 21;
+            button20.Text = "=";
+            button20.UseMnemonic = false;
+            button20.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = SystemColors.ScrollBar;
-            ClientSize = new Size(655, 414);
+            ClientSize = new Size(483, 690);
+            Controls.Add(button20);
+            Controls.Add(button19);
+            Controls.Add(button18);
+            Controls.Add(button17);
+            Controls.Add(button16);
+            Controls.Add(button15);
+            Controls.Add(button14);
+            Controls.Add(button13);
+            Controls.Add(button12);
+            Controls.Add(button11);
+            Controls.Add(button10);
+            Controls.Add(button9);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
             Controls.Add(textBox1);
             Controls.Add(label1);
             Controls.Add(button1);
@@ -85,5 +331,25 @@
         private Button button1;
         private Label label1;
         private TextBox textBox1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button button10;
+        private Button button11;
+        private Button button12;
+        private Button button13;
+        private Button button14;
+        private Button button15;
+        private Button button16;
+        private Button button17;
+        private Button button18;
+        private Button button19;
+        private Button button20;
     }
 }
